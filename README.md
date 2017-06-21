022331acd27a65c610c697383f7954d2a0ef16d1
Mat <majarosz.j50@gmail.com>
2017-06-21 17:34:18 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# EdgeServer
+## Description
+EdgeServer is a Zuul server for sius course.
+
+### Starting
+```
+$ gradlew bootRun
+```
+Server will be available at `localhost:8765`.
---
+++ src/main/java/pl/edu/agh/sius/EdgeApplication.java
@@ -0,0 +1,14 @@
+package pl.edu.agh.sius;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+public class EdgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdgeApplication.class, args);
+	}
+}
